5f38fcb43c78e852775d6d26cb0bc899ff5c11f7
Ortal Wikoff <owikoff@gmail.com>
2022-06-11 00:21:17 +0300

    updated my branch

---
+++ Assets for lyrics/script.js
@@ -0,0 +1,5 @@
+var form = document.getElementById("form");
+var search = document.getElementById("search");
+var result = document.getElementById("result");
+
+var apiURL = "https://api.lyrics.ovh";

8021d1f20507e886513565319d074eeef9541432
Ortal Wikoff <owikoff@gmail.com>
2022-06-10 17:34:33 +0300

    added getElementById and the api URL

---
+++ assets for lyrics/script.js
@@ -0,0 +1,8 @@
+
+
+var form = document.getElementById("form");
+var search = document.getElementById("search");
+var result = document.getElementById("result");
+
+var apiURL = "https://api.lyrics.ovh";
+

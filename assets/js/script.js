2b3e055e5f8d84891cc3aac3bc357782ca7cf81f
Sophia De La Rosa <sophial.delarosa@gmail.com>
2022-06-15 20:48:45 -0400

    Username saves to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,52 @@
 //SOPHIA ENERGY LEVEL SECTION
 
 //variables
-var submitButton = document.getElementById('submit');
 var slider = document.getElementById('slider');
-var client_id = '60d1e4ee22864c28a9dc3afb6e6a08ee';
-var client_secret = '2ad6cb3ca73043d090a00677b2cabd21';
-var access_token = 'BQBKt5WcOsM70IhQ-7gFbf5bdZz9RW2rjZuVTsFHLZz_0k7ik6b3viSbavNp-WU9VwmnZ4qJw04oBNra_fcxsK2YA5iUXBCEPKAVGFt_cnqAjy3cEH5x';
+//clientId = '60d1e4ee22864c28a9dc3afb6e6a08ee';
+//clientSecret = '2ad6cb3ca73043d090a00677b2cabd21';
 
 
+//SOPHIA PROFILE PAGE JS
 
-//function for fetching recommendations with 
-function getRecommendations(event) {
-    //event.preventDefault();
-
-    let energyRecommends = 'https://api.spotify.com/v1/browse/new-releases';
-    // + '?target-energy=' + slider.value
+//CHANGEABLE PROFILE PICTURE
+var loadFile = function (event) {
+    var image = document.getElementById("output");
+    image.src = URL.createObjectURL(event.target.files[0]);
+    };
     
-    fetch(energyRecommends)
-    .then(function (response) {
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function (data) {
-            console.log(data);
-            displayRepos(data, user);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-};
+
+//SAVE USERNAME AND PICTURE INFORMATION
+let saveButton = document.getElementById("save");
 
 
+//function for save button
+function saveProfile () {
+    //establishing variables for the function
+    let profilePic = document.getElementById("output");
+    let userName = document.getElementById("username").value;
+    let profileInfo = " " + profilePic + " " + userName;
+
+    localStorage.setItem('userName', userName);
+
+    //if there is nothing saved at the start then save an empty array
+    if(localStorage.getItem('userName') === null) {
+        localStorage.setItem('userName', '[]');
+    }
+    
+    //get old leaderboard item and slap it to the new data
+    //let oldLeaderboardItem = JSON.parse(localStorage.getItem('leaderboardItem'));
+    //oldLeaderboardItem.push(leaderboardItem);
+
+    //save the old and new leaderboard item in local storage
+    //localStorage.setItem('leaderboardItem', JSON.stringify(oldLeaderboardItem));
+
+    //if local storage profileInfo is not empty, show contents
+    else (
+        userName = (localStorage.getItem(userName))
+    );
 
 
+}
 
+saveButton.addEventListener('click', saveProfile);
 
-
-//main div in results page
-
-
-
-
-//submit event listener
-submitButton.addEventListener('click', getRecommendations);

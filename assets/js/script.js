70198afe713b51cc9a8dfe630ec2fc99d27d4998
Sophia De La Rosa <sophial.delarosa@gmail.com>
2022-06-17 15:46:17 -0400

    Working on saving photo url to local storage and having it display

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ var loadFile = function (event) {
     // const savedURL =JSON.parse(localStorage.getItem('imageURL'));
     // document.getElementById('output').innerHTML=`
     // <img src='${savedURL}'>`
-}
+};
 
 //SAVE USERNAME AND PICTURE INFORMATION
 let saveButton = document.getElementById("save");
@@ -27,21 +27,47 @@ let userName = document.getElementById("username").value;
 function saveProfile (e) {
     e.preventDefault();
     //establishing variables for the function
-    let profilePic = document.getElementById("output");
+    let profilePic = document.getElementById("output").src;
     let userName = document.getElementById("username").value;
     let profileInfo = " " + profilePic + " " + userName;
 
-
+    //Saving username to local storage and displaying it back
     localStorage.setItem('username', JSON.stringify(userName));
     const savedUserName =JSON.parse(localStorage.getItem('username'));
-    document.getElementById('localStorage').innerHTML=`<h4>${savedUserName}</h4>`;
+    document.getElementById('nameFromStorage').innerHTML=`<h1 class="display-4 title">@` + `${savedUserName}</h1>`;
 
-    console.log("*****", document.getElementById("file"))
-    console.log("****", profilePic.img)
+    //saving profile photo URL to localStorage and displaying it
+    localStorage.setItem('photoURL', JSON.stringify(profilePic.slice(5)));
+    const savedPhotoURL = JSON.parse(localStorage.getItem('photoURL'));
+    document.getElementById('output').innerHTML = `<img src="${savedPhotoURL}" id="output" class="output" width="200" />`;
+
+    //console.log("*****", document.getElementById("file"))
+    console.log(profilePic.slice(5))
 
 }
 
-saveButton.onclick =saveProfile
+//if save button is clicked, run saveProfile function
+saveButton.onclick = saveProfile
+
+//IF STATEMENTS to keep username and photo on the page even if refreshed
+//if username in local storage has contents, display username
+if(localStorage.getItem('username') != null) {
+    const savedUserName =JSON.parse(localStorage.getItem('username'));
+    document.getElementById('nameFromStorage').innerHTML=`<h1 class="display-4 title">@` + `${savedUserName}</h1>`;
+};
+
+//if photo URL in local storage has contents, display url
+if(localStorage.getItem('photoURL') != null) {
+    const savedPhotoURL = JSON.parse(localStorage.getItem('photoURL'));
+    document.getElementById('output').innerHTML = `<img src="${savedPhotoURL}" id="output" class="output" width="200" />`;
+}
+
+
+//if username in local storage is empty, display nothing
+//} else if(localStorage.getItem('username') = null) {
+//    document.getElementById('NameFromStorage').innerHTML=``;
+//}
+
 
 //function for favorites button to save to local storage
 
@@ -54,7 +80,4 @@ function saveFav () {
 //song title, artist go to innerHTML text
 //url is put into a tag href
 
-function displayFav () {
-
-}
-
+//function displayFav () {
